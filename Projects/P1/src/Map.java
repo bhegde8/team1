35cd6b2d9db0d131d773b10c6d697a180a4c1d7d
Vi <vivinguyen1992@gmail.com>
2021-01-07 22:41:04 -0500

    Vi Nguyen getLoc Map

--- Projects/P1/src/Map.java
+++ Projects/P1/src/Map.java
@@ -60,7 +60,14 @@ public class Map{
 	
 	public HashSet<Type> getLoc(Location loc) {
 		//wallSet and emptySet will help you write this method
-		return null;
+		if (field.get(loc) == Type.EMPTY ){
+			return emptySet; 
+		}
+		if (field.get(loc) == Type.WALL){
+			return wallSet; 
+		}
+		return field.get(loc);  
+
 	}
 
 	public boolean attack(String Name) {
