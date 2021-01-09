318ac0e2434238a4af1c2157b3a9bd0c645381be
Haley Greenspan <haleygreenspan@gmail.com>
2021-01-09 13:07:28 -0500

    ghost-is_pacman_in_range

--- Projects/P1/src/Ghost.java
+++ Projects/P1/src/Ghost.java
@@ -21,6 +21,18 @@ public class Ghost{
 	}
 
 	public boolean is_pacman_in_range() { 
+		if (this.myMap.getLoc(new Location(this.myLoc.x+1,this.myLoc.y)).contains(Map.Type.PACMAN)) {
+			return true;
+		}
+		if (this.myMap.getLoc(new Location(this.myLoc.x-1,this.myLoc.y)).contains(Map.Type.PACMAN)) {
+			return true;
+		}
+		if (this.myMap.getLoc(new Location(this.myLoc.x,this.myLoc.y+1)).contains(Map.Type.PACMAN)) {
+			return true;
+		}
+		if (this.myMap.getLoc(new Location(this.myLoc.x,this.myLoc.y-1)).contains(Map.Type.PACMAN)) {
+			return true;
+		}
 		return false;
 	}
 
