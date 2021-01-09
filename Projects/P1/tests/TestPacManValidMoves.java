8a8580a46312129f93fa5f38ee6c035456a9c09a
Haley Greenspan <haleygreenspan@gmail.com>
2021-01-09 15:39:37 -0500

    test pacman-get_valid_moves

--- Projects/P1/tests/TestPacManValidMoves.java
+++ Projects/P1/tests/TestPacManValidMoves.java
@@ -1,15 +1,20 @@
 import junit.framework.*;
 import java.awt.Color;
 import java.io.*;
+import java.util.ArrayList;
+
+import javax.tools.DocumentationTool.Location;
 
 public class TestPacManValidMoves extends TestCase {
 
 	public void testPacManValidMoves() throws FileNotFoundException{
 		Mainframe frame = new MainFrame();
-		PacMan pacman = frame.addPacMan(new Location(2, 2));
-		Ghost ghost = frame.addGhost(new Location(6, 6), "g1", Color.red);
+		PacMan pacman = frame.addPacMan(new Location(1, 1));
+		Ghost ghost = frame.addGhost(new Location(5, 5), "g1", Color.red);
 		frame.startGame();
-		Arraylist<Location> expected = {new Location(3,2),new Location(2,3)};
+		ArrayList<Location> expected = new ArrayList<Location>();
+		expected.add(new Location(2,1));
+		expected.add(new Location(1,2));
 		assertEquals(expected, pacman.get_valid_moves());
 		
 	}
