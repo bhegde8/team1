f3fc1608b4f07011fb4077c3d1b14ea9b30b9887
Haley Greenspan <haleygreenspan@gmail.com>
2021-01-09 14:47:33 -0500

    pacman-get_valid_moves test

--- Projects/P1/tests/TestPacManValidMoves.java
+++ Projects/P1/tests/TestPacManValidMoves.java
@@ -5,6 +5,12 @@ import java.io.*;
 public class TestPacManValidMoves extends TestCase {
 
 	public void testPacManValidMoves() throws FileNotFoundException{
-		return null;
+		Mainframe frame = new MainFrame();
+		PacMan pacman = frame.addPacMan(new Location(2, 2));
+		Ghost ghost = frame.addGhost(new Location(6, 6), "g1", Color.red);
+		frame.startGame();
+		Arraylist<Location> expected = {new Location(3,2),new Location(2,3)};
+		assertEquals(expected, pacman.get_valid_moves());
+		
 	}
 }
