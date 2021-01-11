ea6dc002014ac9243a9994ad5bad7d8b1b52232b
Vi <vivinguyen1992@gmail.com>
2021-01-11 03:44:32 -0500

    Vi Nguyen map-move

--- Projects/P1/src/Map.java
+++ Projects/P1/src/Map.java
@@ -55,7 +55,38 @@ public class Map{
 	public boolean move(String name, Location loc, Type type) {
 		//update locations, components, and field
 		//use the setLocation method for the component to move it to the new location
+		
+		Location old = locations.get(name);	 
+        if (type == Type.PACMAN){
+			PacMan pacman = new Pacman(name, old, this);
+		
+			if (pacman.get_valid_move().contains(loc)){
+				locations.put(name, loc);
+				components.get(name).setLocation(loc.x, loc.y);
+
+				field.get(loc).add(type);  
+				return true;
+
+			}
+			
+		}
+
+		if (type == Type.GHOST){
+			Ghost ghost = new Ghost(name, old, this);
+		
+			if (ghost.get_valid_move().contains(loc)){
+				locations.put(name, loc);
+				components.get(name).setLocation(loc.x, loc.y);
+
+				field.get(loc).add(type);  
+				return true;
+			}
+			
+		}
 		return false;
+
+			
+		
 	}
 	
 	public HashSet<Type> getLoc(Location loc) {
