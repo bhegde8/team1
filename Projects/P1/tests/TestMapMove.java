9e12dbc9bf0b4738af2b5777a464d8151a4a499e
Vi <vivinguyen1992@gmail.com>
2021-01-11 03:51:51 -0500

    Vi Nguyen add testMapMove

--- Projects/P1/tests/TestMapMove.java
+++ Projects/P1/tests/TestMapMove.java
@@ -5,6 +5,12 @@ import java.io.*;
 public class TestMapMove {
 
 	public void testMapMove() {
-		return null;
+		
+		NoFrame frame = new NoFrame();
+		Map map = frame.getMap();
+
+		Ghost ghost = frame.addGhost(new Location(4,5), "Ghost", Color.red);	
+		PacMan pacman = frame.addPacMan(new Location(8,10));
+		assertTrue(map.move("Ghost", new Location (5,5), Map.Type.GHOST));
 	}
 }
