a01d9b72ffa389bcbc00fb4cd1c3d16ebeb06ab0
Vi <vivinguyen1992@gmail.com>
2021-01-10 23:37:16 -0500

    VN fix syntax

--- Projects/P1/tests/TestMapGetLoc.java
+++ Projects/P1/tests/TestMapGetLoc.java
@@ -6,10 +6,10 @@ import java.io.*;
 public class TestMapGetLoc {
 	
 	public void testMapGetLoc() {
-		Mainframe frame = new MainFrame();
-		Map map = new Map(64);
-		Ghost ghost = frame.addGhost(new Location(2,4), "Luke", Color.red);
-		PacMan pacman = frame.addPacMan(new Location(4,6));
+		NoFrame frame = new NoFrame();
+		Map map = frame.getMap();
+		frame.addGhost(new Location(2,4), "Luke", Color.red);
+		frame.addPacMan(new Location(4,6));
 
 		assertEquals(map.getLoc(new Location (2,4)), Map.Type.GHOST); 
 		assertEquals(map.getLoc(new Location (4,6)), Map.Type.PACMAN);
