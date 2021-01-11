886d364ed1b380c0c529fba478307e71dba13ebf
XavierW10git config --global user.name XavierW10 <xavwar1@gmail.com>
2021-01-11 18:02:24 -0500

    Updated Ghost-valid_moves test

--- Projects/P1/tests/TestGhostValidMoves.java
+++ Projects/P1/tests/TestGhostValidMoves.java
@@ -7,8 +7,7 @@ public class TestGhostValidMoves extends TestCase {
 
 	public void testGhostValidMoves() throws FileNotFoundException{
 		NoFrame frame = new NoFrame();		
-		Ghost ghost = frame.addGhost(new Location(5, 5), "ghost1", Color.red);
-		PacMan pacman = frame.addPacMan(new Location(1, 1));
+		Ghost ghost = frame.addGhost(new Location(1, 1), "ghost1", Color.red);
 		ArrayList<Location> expected = new ArrayList<Location>();
 		expected.add(new Location(2,1));
 		expected.add(new Location(1,2));
