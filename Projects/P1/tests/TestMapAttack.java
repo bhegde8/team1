bfdc6e6b17337c27998fdf05ab9eb86ac21822d3
Vi <vivinguyen1992@gmail.com>
2021-01-11 19:52:37 -0500

    update MapAttact test

--- Projects/P1/tests/TestMapAttack.java
+++ Projects/P1/tests/TestMapAttack.java
@@ -4,6 +4,12 @@ import junit.framework.*;
 public class TestMapAttack {
 
 	public void testMapAttack() {
-		return null;	
+		NoFrame frame = new NoFrame();
+		Map mymap = frame.getMap();
+		PacMan pacman = frame.addPacMan(new Location(1,1));
+		Ghost ghost1 = frame.addGhost(new Location(2,1), "Ghost1", Color.red);
+		Ghost ghost2 = frame.addGhost(new Location(3,3), "Ghost2", Color.red);
+		assertTrue(mymap.attack("Ghost1"));	
+		assertFalse(mymap.attack("Ghost2"));
 	}
 }
